d0a1e000ca3f72add8fc54d3ee33383b8032ab51
Gigin George <mail.gigin@gmail.com>
2022-02-20 11:29:36 +0530

    Switch to WhiteNoise for production

--- config/settings/production.py
+++ config/settings/production.py
@@ -87,7 +87,7 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "course_api.utils.storages.StaticRootS3Boto3Storage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
@@ -204,3 +204,4 @@ sentry_sdk.init(
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+MIDDLEWARE+=["whitenoise.middleware.WhiteNoiseMiddleware"]
