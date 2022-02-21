8305707b566833d7cbf44b5522e2eec51acfdb9d
Gigin George <mail.gigin@gmail.com>
2022-02-21 15:42:03 +0530

    Add Basic Authentication

--- config/settings/base.py
+++ config/settings/base.py
@@ -307,6 +307,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticatedOrReadOnly",),
